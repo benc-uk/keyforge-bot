cce6f9cd07c1ee2f38e22526260c2f05d3e0d2fe
Ben Coleman <becolem@microsoft.com>
2018-11-12 09:53:59 +0000

    Cleanup

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
  
 import praw
-import os
 import re
 import urllib
 import sqlite3
+import signal
+import sys
+from datetime import datetime
 from dotenv import load_dotenv
 from pprint import pprint
 
-DB_NAME = 'replies.db'
+DB_NAME        = '/data/replies.db'
 SUBREDDIT_NAME = 'testingground4bots'
-CARD_SITE_URL = "http://aembertree.com/search?q={}"
+CARD_SITE_URL  = "http://aembertree.com/search?q={}"
+SKIP_OLD       = True
 
 print("### Starting KeyForgeCardBot...")
 
@@ -25,28 +28,35 @@ print("### Listening to new comments in /r/{}".format(SUBREDDIT_NAME))
 dbconn = sqlite3.connect(DB_NAME)
 dbconn.cursor().execute('CREATE TABLE IF NOT EXISTS replied_to (comment_id text)')
 
-def commentReplied(id):
+def checkAlreadyReplied(id):
   cur = dbconn.cursor()
   cur.execute("SELECT COUNT(*) FROM replied_to WHERE comment_id = ?", [id])
-  return (cur.fetchone()[0] > 0)
+  return cur.fetchone()[0] > 0
 
 def saveReply(id):
   dbconn.cursor().execute("INSERT INTO replied_to VALUES (?)", [id])
   dbconn.commit()
 
+def sigintHandler(sig, frame):
+  print('### Closing...')
+  dbconn.close()
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, sigintHandler)
+
 pattern = re.compile(r'\[\[(.*?)\]\]', flags=re.M|re.I)
 
-for comment in subreddit.stream.comments():
-  if not commentReplied(comment.id) and comment.author.name != "KeyForgeCardBot":
+for comment in subreddit.stream.comments(skip_existing=SKIP_OLD):
+  if not checkAlreadyReplied(comment.id) and comment.author.name != "KeyForgeCardBot":
     matches = pattern.findall(comment.body)
     if matches:
       reply = ""
+      print("### {} - Processing comment {} by {}".format(datetime.fromtimestamp(comment.created_utc).strftime('%d-%m-%Y %H:%M'), comment.id, comment.author.name))
       for card_name in matches:
         card_name_encoded = urllib.parse.quote_plus(card_name)
         url = CARD_SITE_URL.format(card_name_encoded)
         reply += "- [{}]({})".format(card_name, url) + "\n"
 
-      print("### Made a reply\n", reply)
       reply += "\n^I ^am ^a ^bot. ^Put ^[[cardname]] ^into ^your ^comment ^to ^call ^me"
 
       try:
@@ -55,5 +65,5 @@ for comment in subreddit.stream.comments():
         saveReply(comment.id)
         print("### OK!\n")
       except Exception as e:
-        print(e)
+        print("!!!", e)
 
